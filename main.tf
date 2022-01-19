901393c80851c7417da80562b2b65f1d1a2c9679
waldek andrukiewicz <waldek.social@pm.me>
2022-01-19 22:46:10 +0100

    azure scaleset with autoscale
    initial setup combined from files provided by Hashicorp used to test issues with rolling upgrade

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
   }
 
   network_interface {
-    name    = "example"
+    name    = "main"
     primary = true
 
     ip_configuration {
@@ -122,4 +122,79 @@ resource "azurerm_linux_virtual_machine_scale_set" "main" {
   }
 
   depends_on = [azurerm_lb_rule.main]
+}
+
+resource "azurerm_monitor_autoscale_setting" "main" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.main.id
+
+  profile {
+    name = "Weekends"
+
+    capacity {
+      default = 5
+      minimum = 1
+      maximum = 10
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT2M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 90
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT2M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT1M"
+      }
+    }
+/*
+    recurrence {
+      frequency = "Week"
+      timezone  = "Pacific Standard Time"
+      days      = ["Saturday", "Sunday"]
+      hours     = [12]
+      minutes   = [0]
+    }
+
+*/
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = true
+      custom_emails                         = ["admin@contoso.com"]
+    }
+  }
 }
